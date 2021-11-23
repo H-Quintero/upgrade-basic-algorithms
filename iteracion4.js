c6ea3420a742910207651fbc0bacc9bd7b5aa085
Hugo Quintero <hugoquinterogarcia@gmail.com>
2021-11-23 10:21:04 +0100

    Iteraciones 4, 5, 6

---
+++ iteracion4.js
@@ -0,0 +1,29 @@
+//1.1
+const avengers = ['HULK', 'SPIDERMAN', 'BLACK PANTHER']
+console.log(avengers[0])
+
+//1.2
+avengers[0] = 'IRON MAN'
+console.log(avengers[0])
+
+//1.3
+alert(avengers.length)
+
+//1.4
+const rickAndMortyCharacters = ['Rick', 'Beth', 'Jerry']
+    rickAndMortyCharacters.push('Morty', 'Summer')
+    console.log(rickAndMortyCharacters[4])
+
+//1.5
+
+const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]
+    rickAndMortyCharacters1.pop()
+    console.log(rickAndMortyCharacters1)
+    console.log(rickAndMortyCharacters1[0])
+    console.log(rickAndMortyCharacters1[4])
+
+//1.6
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]
+    rickAndMortyCharacters2.splice(2,1)
+    console.log(rickAndMortyCharacters2)
---
+++ iteracion6.js
@@ -0,0 +1,20 @@
+//1.1
+/* for(let i = 0; i <= 9; i++) {
+    console.log(i)
+} */
+
+//1.2
+/* for(let x = 0; x <= 9; x++) {
+    if(x % 2 == 0) {    
+        console.log(x)
+    }
+} */
+
+//1.3
+for(let y = 0; y <= 10; y++) {
+    if(y < 10) {
+        console.log('Intentando dormir')
+    } else {
+        console.log('Dormido!')
+    }
+}
